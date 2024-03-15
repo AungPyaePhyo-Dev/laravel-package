6373d4b4177fa155912a524f3f34de83de57c2e6
Aung Pyae Phyo <aungpyaephyoe.dev@gmail.com>
2024-03-15 16:49:10 +0630

    update version 1.1.1

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
     Route::get('calc', function() {
         echo "Calculator starting!";
-    });
+    }); 
 
     Route::get('add/{a}/{b}', [Aung\Calc\CalculatorController::class, 'add']);
     Route::get('substract/{a}/{b}', [Aung\Calc\CalculatorController::class, 'substract']);
